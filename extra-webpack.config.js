1abf059e90e917d8b5104404efcc18c66f37a63d
Artur Ribeiro <artur.adr@hotmail.com>
2020-04-02 23:46:49 -0300

    config dpa

---
+++ extra-webpack.config.js
@@ -0,0 +1,10 @@
+const singleSpaAngularWebpack = require('single-spa-angular/lib/webpack').default
+
+module.exports = (angularWebpackConfig, options) => {
+  const singleSpaWebpackConfig = singleSpaAngularWebpack(angularWebpackConfig, options)
+
+  angularWebpackConfig.output.library = 'transactions';
+
+  // Feel free to modify this webpack config however you'd like to
+  return singleSpaWebpackConfig
+}
