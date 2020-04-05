98b650c442fab15e640e23b823d466612af12515
Artur Ribeiro <artur.adr@hotmail.com>
2020-04-05 17:26:01 -0300

    finalizando projeto

--- src/app/services/transaction.service.ts
+++ src/app/services/transaction.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+
 import { environment } from 'src/environments/environment';
 import { Params } from '../models/params';
 import { Transaction } from '../models/transaction';
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  urlIncome: 'https://us-central1-controlagasto-da53a.cloudfunctions.net'
 };
